7b05277910a2d404e7b80fe3e55ebe6208173d56
Alexander Schrepfer <schrepfer@google.com>
2022-11-09 10:42:08 -0800

    Cleaned up the code. Better flag comments. Added difflib.

--- README.md
+++ README.md
@@ -4,27 +4,48 @@ My REPlace (MREP): Replaces occurrences of text within a file.
 ## Usage
 
 ```
-usage: mrep.py [-h] [-v LEVEL] [-V] [-b] [--backup_format FORMAT] [-r] [-n] [-f RegexFlag] [-e] SEARCH REPLACEMENT FILE [FILE ...]
+usage: mrep.py [-h] [-v LEVEL] [-V] [-b] [--backup_format FORMAT] [-r] [-n]
+               [-f RegexFlag] [-e]
+               SEARCH REPLACEMENT FILE [FILE ...]
 
 My REPlace (MREP): Replaces occurrences of text within a file.
 
 positional arguments:
-  SEARCH                search string or pattern (with --regexp)
-  REPLACEMENT           replacement string
-  FILE                  files to consider
+  SEARCH                Search string or pattern.
+  REPLACEMENT           Replacement string. If --regexp is enabled, you can
+                        reference capturing groups with \1, \2, etc.
+  FILE                  Files to consider in the search replacement.
 
 options:
   -h, --help            show this help message and exit
   -v LEVEL, --verbosity LEVEL
-                        the logging verbosity
+                        The logging verbosity.
   -V, --version         show program's version number and exit
-  -b, --backup          backup files being modified
+  -b, --backup          Backup files being modified. See --backup_format for
+                        options on the format.
   --backup_format FORMAT
-                        backup files in this format; %s is expanded
+                        Backup files in this format; {'option_strings': ['--
+                        backup_format'], 'dest': 'backup_format', 'nargs':
+                        None, 'const': None, 'default': '%s~', 'type': 'str',
+                        'choices': None, 'required': False, 'help': 'Backup
+                        files in this format; %s is expanded to the current
+                        file name.', 'metavar': 'FORMAT', 'container':
+                        <argparse._ArgumentGroup object at 0x7f05b11b2770>,
+                        'prog': 'mrep.py'} is expanded to the current file
+                        name.
   -r, --regexp, --regex, --re
-                        make the search string a regexp pattern
-  -n, --pretend         only pretend to do the replacements
+                        Make the search string a regexp pattern. With this
+                        option you can use regexp capturing groups, and
+                        reference those values in the replacement with \1, \2,
+                        etc.
+  -n, --pretend         Only pretend to do the replacements, but do not
+                        actually do it.
   -f RegexFlag, --flags RegexFlag
-                        See https://docs.python.org/3/library/re.html#re.RegexFlag for options
-  -e, --backslash       enable usage of backslash escapes
+                        See
+                        https://docs.python.org/3/library/re.html#re.RegexFlag
+                        for options. RegexFlags: re.ASCII, re.DEBUG,
+                        re.DOTALL, re.IGNORECASE, re.LOCALE, re.MULTILINE,
+                        re.NOFLAG, re.VERBOSE
+  -e, --backslash       Enable usage of backslash escapes. Useful if you want
+                        to replace \r, etc.
 ```
--- mrep.py
+++ mrep.py
@@ -3,6 +3,7 @@
 """My REPlace (MREP): Replaces occurrences of text within a file."""
 
 import argparse
+import difflib
 import itertools
 import logging
 import os
@@ -11,7 +12,19 @@ import shutil
 import sys
 
 
-def defineFlags():
+flagsChoices = [
+    're.ASCII',      're.A',
+    're.DEBUG',
+    're.DOTALL',     're.S',
+    're.IGNORECASE', 're.I',
+    're.LOCALE',     're.L',
+    're.MULTILINE',  're.M',
+    're.NOFLAG',
+    're.VERBOSE',    're.X',
+]
+
+
+def defineFlags() -> argparse.Namespace:
   parser = argparse.ArgumentParser(description=__doc__)
   # See: http://docs.python.org/3/library/argparse.html
   parser.add_argument(
@@ -19,82 +32,85 @@ def defineFlags():
       action='store',
       default=20,
       type=int,
-      help='the logging verbosity',
+      help='The logging verbosity.',
       metavar='LEVEL')
   parser.add_argument(
       '-V', '--version',
       action='version',
-      version='%(prog)s version 0.1')
+      version='%(prog)s version 0.2')
   parser.add_argument(
       '-b', '--backup',
       action='store_true',
       default=False,
-      help='backup files being modified')
+      help='Backup files being modified. See --backup_format for options on the format.')
   parser.add_argument(
       '--backup_format',
       action='store',
       default='%s~',
       type=str,
-      help='backup files in this format; %%s is expanded',
+      help='Backup files in this format; %s is expanded to the current file name.',
       metavar='FORMAT')
   parser.add_argument(
       '-r', '--regexp', '--regex', '--re',
       action='store_true',
       default=False,
-      help='make the search string a regexp pattern')
+      help=(
+          'Make the search string a regexp pattern. With this option you can use regexp capturing '
+          r'groups, and reference those values in the replacement with \1, \2, etc.'),
+  )
   parser.add_argument(
-      '-n', '--pretend',
+      '-n', '--diff',
       action='store_true',
       default=False,
-      help='only pretend to do the replacements')
+      help='Diff the proposed changes only. Do not actually touch the files.',
+  )
   parser.add_argument(
       '-f', '--flags',
       action='append',
-      choices=[
-          're.ASCII',      're.A',
-          're.DEBUG',
-          're.DOTALL',     're.S',
-          're.IGNORECASE', 're.I',
-          're.LOCALE',     're.L',
-          're.MULTILINE',  're.M',
-          're.NOFLAG',
-          're.VERBOSE',    're.X',
-      ],
+      choices=flagsChoices,
       nargs=1,
       type=str,
       metavar='RegexFlag',
-      help='See https://docs.python.org/3/library/re.html#re.RegexFlag for options')
+      help=(
+          'See https://docs.python.org/3/library/re.html#re.RegexFlag for options. '
+          'RegexFlags: ' + ', '.join(filter(lambda x: len(x) > 4, flagsChoices))),
+  )
   parser.add_argument(
       '-e', '--backslash',
       action='store_true',
       default=False,
-      help='enable usage of backslash escapes')
-
+      help='Enable usage of backslash escapes. Useful if you want to replace \\r, etc.',
+  )
   parser.add_argument(
       'search',
       nargs=1,
       type=str,
-      help='search string or pattern (with --regexp)',
-      metavar='SEARCH')
+      help='Search string or pattern.',
+      metavar='SEARCH',
+  )
   parser.add_argument(
       'replacement',
       nargs=1,
       type=str,
-      help='replacement string',
-      metavar='REPLACEMENT')
+      help=(
+          'Replacement string. If --regexp is enabled, you can reference capturing groups with '
+          r'\1, \2, etc.'),
+      metavar='REPLACEMENT',
+  )
   parser.add_argument(
       'files',
       nargs='+',
       type=str,
-      help='files to consider',
-      metavar='FILE')
+      help='Files to consider in the search replacement.',
+      metavar='FILE',
+  )
 
   args = parser.parse_args()
   checkFlags(parser, args)
   return args
 
 
-def checkFlags(parser, args):
+def checkFlags(parser: argparse.ArgumentParser, args: argparse.Namespace) -> None:
   # See: http://docs.python.org/3/library/argparse.html#exiting-methods
   if args.backup_format and args.backup_format.count('%s') != 1:
     parser.error('--backup_format must contain %s exactly once')
@@ -106,7 +122,7 @@ def checkFlags(parser, args):
     parser.error('--flags specified but --regexp is not')
 
 
-def regexpFlags(args):
+def regexpFlags(args: argparse.Namespace) -> int:
   flags = 0
   for f in itertools.chain.from_iterable(args.flags):
     flags |= eval(f).value
@@ -116,11 +132,14 @@ def regexpFlags(args):
 class Replacer(object):
   """Replacer keeps state and replaces content in files."""
 
-  def __init__(self, args):
+  args: argparse.Namespace
+  flags: int
+
+  def __init__(self, args: argparse.Namespace):
     self.args = args
     self.flags = regexpFlags(args)
 
-  def replaceOne(self, file_path):
+  def replaceOne(self, file_path: str) -> bool:
     """Replace text in the given file."""
     if not os.path.isfile(file_path):
       logging.error('Not a valid/readable file: %s', file_path)
@@ -128,17 +147,17 @@ class Replacer(object):
 
     try:
       with open(file_path, 'r') as file_handle:
-        file_contents = file_handle.read()
+        file_contents: str = file_handle.read()
     except:
       logging.error('Could not open file for reading: %s', file_path)
       return False
 
-    search = self.args.search[0]
-    replacement = self.args.replacement[0]
+    search: str = self.args.search[0]
+    replacement: str = self.args.replacement[0]
 
     if self.args.backslash:
-      search = search.decode('string_escape')
-      replacement = replacement.decode('string_escape')
+      search = search.encode('utf-8').decode('unicode_escape')
+      replacement = replacement.encode('utf-8').decode('unicode_escape')
 
     if self.args.regexp:
       new_file_contents = re.sub(search, replacement, file_contents, flags=self.flags)
@@ -149,30 +168,36 @@ class Replacer(object):
       logging.warning('Nothing replaced: %s', file_path)
       return True
 
-    if self.args.backup and self.args.backup_format:
-      backup_file_path = self.args.backup_format % file_path
+    if self.args.backup and self.args.backup_format and not self.args.diff:
+      backup_file_path: str = self.args.backup_format % file_path
       try:
-        if not self.args.pretend:
-          shutil.copyfile(file_path, backup_file_path)
+        shutil.copyfile(file_path, backup_file_path)
         logging.info('Created backup for %s: %s', file_path, backup_file_path)
       except:
         logging.error('Could not create backup for %s, skipping: %s', file_path, backup_file_path)
         return False
 
-    if not self.args.pretend:
-      try:
-        file_handle = open(file_path, 'w')
-      except:
-        logging.error('Could not open file for writing: %s', file_path)
-        return False
-      with file_handle:
-        file_handle.write(new_file_contents)
+    if self.args.diff:
+      print('\n'.join(difflib.unified_diff(
+          file_contents.split('\n'),
+          new_file_contents.split('\n'),
+          fromfile='before',
+          tofile='after',
+      )))
+      return True
 
+    try:
+      file_handle = open(file_path, 'w')
+    except:
+      logging.error('Could not open file for writing: %s', file_path)
+      return False
+    with file_handle:
+      file_handle.write(new_file_contents)
     logging.info('Replaced %r with %r in %s', search, replacement, file_path)
 
     return True
 
-  def replace(self):
+  def replace(self) -> bool:
     """Replace text in all files."""
     errors = False
     for file_path in self.args.files:
@@ -181,7 +206,7 @@ class Replacer(object):
     return not errors
 
 
-def main(args):
+def main(args: argparse.Namespace) -> int:
   if not Replacer(args).replace():
     return os.EX_DATAERR
 
